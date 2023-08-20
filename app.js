971ca0398e08f4f8fba846030a4da873559826fb
Nur Ahamed Mondal <nurahamed62@gmail.com>
2023-08-20 16:53:48 +0530

    input processing output

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
-console.log(txtInput);
-
 var outputDiv = document.querySelector("#output");
+// console.log(txtInput);
+
 
 
 function clickHandler() {
-  console.log("Clicked");
-  console.log("Input value is: ",  txtInput.value);
+//   console.log("Clicked");
+//   console.log("Input value is: ",  txtInput.value);
   outputDiv.innerHTML = txtInput.value;
 };
 
